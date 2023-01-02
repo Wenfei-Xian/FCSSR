cd4718d0095f42b072634e0d21106a1777f5e66d
Wenfei Xian <2316469036@qq.com>
2023-01-02 14:06:25 +0100

    Delete fuzzy.h

--- fuzzy.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "sequence.format.h"
-#include <unordered_map>
-#include <map>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-void find_fuzzy(string DNA, string id, int unitlen_p, unsigned int ssrlen_p, int insertion_p, float unmatch_ssr_p, int both_p, int up_p, unsigned int flanking_p, int unmatch_num_p, int revercomple_p );
